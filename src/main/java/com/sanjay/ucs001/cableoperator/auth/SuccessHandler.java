915cead6d58f0e4a3f19459bef558e53d639d8a5
Akhil Robert <akhilrobert2412@gmail.com>
2022-03-05 09:58:41 +0530

    closes #1

--- src/main/java/com/sanjay/ucs001/cableoperator/auth/SuccessHandler.java
+++ src/main/java/com/sanjay/ucs001/cableoperator/auth/SuccessHandler.java
@@ -1,6 +1,5 @@
 package com.sanjay.ucs001.cableoperator.auth;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.RedirectStrategy;
@@ -13,7 +12,6 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
-@Slf4j
 public class SuccessHandler implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request,
